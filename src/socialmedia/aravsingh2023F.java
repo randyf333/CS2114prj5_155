dc6f3a37a92f03b1cb7a1bedc72fe5ca499a23b7
ethanbrownvt <149278016+ethanbrownvt@users.noreply.github.com>
2023-11-03 15:15:15 -0400

    added ethan to arav.java
    added a hello from ethan on arav's java file

--- src/socialmedia/aravsingh2023F.java
+++ src/socialmedia/aravsingh2023F.java
@@ -1,6 +1,10 @@
 package socialmedia;
 public class aravsingh2023F
 {
+    public static void main(String[] args)
+    {
+        System.out.println("hello from ethan");
+    }
     //~ Fields ................................................................
 
     //~ Constructors ..........................................................
