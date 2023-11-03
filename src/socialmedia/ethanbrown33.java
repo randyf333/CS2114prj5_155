d87c685fa57e912e42884966448b4eda24105272
ethanbrownvt <149278016+ethanbrownvt@users.noreply.github.com>
2023-11-03 14:43:30 -0400

    Create ethanbrown33.java
    Added ethanbrown33.java for my submission

---
+++ src/socialmedia/ethanbrown33.java
@@ -0,0 +1,24 @@
+package socialmedia;
+
+// -------------------------------------------------------------------------
+/**
+ * Github demo class
+ * 
+ * @author ethan
+ * @version Nov 3, 2023
+ */
+public class ethanbrown33
+{
+
+    // ----------------------------------------------------------
+    /**
+     * Program start
+     * 
+     * @param args
+     *            string of arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World from Ethan");
+    }
+}
