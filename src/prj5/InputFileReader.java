2ddbc9dc2d97e16ebb5e46fbb4dee688b8979df2
aravsingh2023F <aravsingh@vt.edu>
2023-11-15 13:33:10 -0500

    Added ProjectRunner so that the code can run

--- src/prj5/InputFileReader.java
+++ src/prj5/InputFileReader.java
@@ -1,15 +1,50 @@
 package prj5;
-//-------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------
 /**
-* This class is used to parse the input files
-* 
-* @author aravs
-* @version Nov 15, 2023
-*/
+ * This class is used to parse the input files
+ * 
+ * @author aravs
+ * @version Nov 15, 2023
+ */
 public class InputFileReader
 {
- // ~ Fields ................................................................
+    // ~ Fields ................................................................
+    private String inputFile;
 
- // ~ Constructors ..........................................................
+    // ~ Constructors ..........................................................
+    /**
+     * Creates a new InputFileReader object
+     * 
+     * @param input
+     *            Name of the file that needs to be parsed
+     */
+    public InputFileReader(String input)
+    {
+        inputFile = input;
+    }
 
- // ~Public Methods ........................................................
+
+    // ~Private Methods ........................................................
+    /**
+     * Converts a string to an integer, throws an exception if the argument can
+     * not be converted to an integer
+     * 
+     * @param str
+     *            String that needs to be converted
+     */
+    private int toInt(String str)
+    {
+
+        try
+        {
+            return Integer.ParseInt(str);
+        }
+        catch (Exception e)
+        {
+            return 0;
+        }
+    }
+    // ~Public Methods ........................................................
+
+}
---
+++ src/prj5/ProjectRunner.java
@@ -0,0 +1,38 @@
+package prj5;
+
+// -------------------------------------------------------------------------
+/**
+ * Runs the project
+ * 
+ * @author aravs
+ * @version Nov 15, 2023
+ */
+public class ProjectRunner
+{
+    // ~ Fields ................................................................
+
+    // ~ Constructors ..........................................................
+
+    // ~Public Methods ........................................................
+    /**
+     * Method to run the code
+     *
+     * @param args
+     *            Argument, if not empty, that contains the file needed to be
+     *            parsed
+     */
+    public static void main(String[] args)
+        throws IOException
+    {
+        InputFileReader filer;
+
+        if (args.length > 0)
+        {
+            filer = new InputFileReader(args[0]);
+        }
+        else
+        {
+            filer = new InputFileReader("SampleInput1_2023.csv");
+        }
+    }
+}
