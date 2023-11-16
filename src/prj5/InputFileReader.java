7f36f6b81b2a3799995218af2021915815966fee
aravsingh2023F <aravsingh@vt.edu>
2023-11-16 17:34:23 -0500

    Added test cases for SinglyLinkedList

--- src/prj5/InputFileReader.java
+++ src/prj5/InputFileReader.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 /**
  * This class is used to parse the input files
  * 
- * @author aravs
+ * @author Team 155
  * @version Nov 15, 2023
  */
 public class InputFileReader
